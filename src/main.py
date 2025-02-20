39eabd4572b4fb574d90730056354b8f8a21161c
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-02-19 23:39:41 -0500

    created main.py file to print out and compare created nodes

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textnode import TextNode, TextType
+
+def main():
+    node1 = TextNode("Hello World", TextType.NORMAL)
+    node2 = TextNode("Hello World", TextType.BOLD)
+
+    # Print the nodes (this automatically uses __repr__)
+    print(node1)
+    print(node2)
+
+    # Compare nodes (this automatically uses __eq__)
+    print(node1 == node2)
+
+main()
